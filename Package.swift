1d27d60c1af90b5b37fe9240b43f3f5ac305e361
Francesco Bigagnoli <fbigagnoli@spotify.com>
2024-01-17 17:43:53 +0100

    Enable concurrency warnings

--- Package.swift
+++ Package.swift
@@ -1,13 +1,13 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.8
 import PackageDescription
 
 let package = Package(
     name: "Mobius",
     platforms: [
-        .iOS(.v10),
-        .macOS(.v10_12),
-        .tvOS(.v10),
-        .watchOS(.v3),
+        .iOS(.v13),
+        .macOS(.v10_13),
+        .tvOS(.v12),
+        .watchOS(.v4),
     ],
     products: [
         .library(name: "MobiusCore", targets: ["MobiusCore"]),
@@ -16,13 +16,17 @@ let package = Package(
         .library(name: "MobiusTest", targets: ["MobiusTest"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-case-paths", .upToNextMinor(from: "0.10.1")),
+        .package(url: "https://github.com/pointfreeco/swift-case-paths", .upToNextMinor(from: "1.2.1")),
         .package(url: "https://github.com/Quick/Nimble", from: "10.0.0"),
         .package(url: "https://github.com/Quick/Quick", from: "5.0.1"),
     ],
     targets: [
-        .target(name: "MobiusCore", dependencies: ["CasePaths"], path: "MobiusCore/Source"),
-        .target(name: "MobiusExtras", dependencies: ["MobiusCore"], path: "MobiusExtras/Source"),
+        .target(name: "MobiusCore", dependencies: [.product(name: "CasePaths", package: "swift-case-paths")], path: "MobiusCore/Source", swiftSettings: [
+            .enableExperimentalFeature("StrictConcurrency"),
+        ]),
+        .target(name: "MobiusExtras", dependencies: ["MobiusCore"], path: "MobiusExtras/Source", swiftSettings: [
+            .enableExperimentalFeature("StrictConcurrency"),
+        ]),
         .target(name: "MobiusNimble", dependencies: ["MobiusCore", "MobiusTest", "Nimble"], path: "MobiusNimble/Source"),
         .target(name: "MobiusTest", dependencies: ["MobiusCore"], path: "MobiusTest/Source"),
         .target(name: "MobiusThrowableAssertion", path: "MobiusThrowableAssertion/Source"),
